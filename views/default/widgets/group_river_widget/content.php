b31878fd3e31749ec3674678d30be00b6edbbeab
jdalsem <jdalsem@coldtrick.com>
2012-05-11 13:34:34 +0200

    added: filter and group multiselect to the widget

--- views/default/widgets/group_river_widget/content.php
+++ views/default/widgets/group_river_widget/content.php
@@ -1,52 +1,28 @@
 <?php
 
-	global $CONFIG;
-
 	$widget = $vars["entity"];
 	
 	// which group
-	if($widget->context != "groups"){
-// 		if($group_guid = $widget->getMetadata("group_guid")){
-// 			if(!is_array($group_guid)){
-// 				$group_guid = array($group_guid);
-// 			}
-// 		} elseif($group_guid = $widget->group_guid) {
-// 			$group_guid = array($group_guid);
-// 		} else {
-// 			$group_guid = array();
-// 		}
-		
-		if($group_guid = $widget->group_guid) {
-			$group_guid = array($group_guid);
-		} else {
-			$group_guid = array();
-		}
-	} else {
+	if($widget->context == "groups"){
 		$group_guid = array($widget->getOwnerGUID());
-	}
+	} else {
+		$group_guid = $widget->group_guid;
+		if(!empty($group_guid)){
+			if(!is_array($group_guid)){
+				$group_guid = array($group_guid);
+			}
+		}
+	} 
 	
 	$group_guid = array_map("sanitise_int", $group_guid);
 	if(($key = array_search(0, $group_guid)) !== false){
 		unset($group_guid[$key]);
 	}
 	
-	// get activity filter
-// 	$activity_filter = $widget->getMetadata("activity_filter");
-// 	if(empty($activity_filter)){
-// 		// fallback to old situation
-// 		$activity_filter = $widget->activity_filter;
-// 		if($activity_filter == "all"){
-// 			$activity_filter = array();
-// 		}
-// 	}
-	
-// 	if(!empty($activity_filter) && !is_array($activity_filter)){
-// 		$activity_filter = array($activity_filter);
-// 	} elseif(empty($activity_filter)){
-// 		$activity_filter = array();
-// 	}
-	
 	if(!empty($group_guid)){
+		// get activity filter
+		$activity_filter = $widget->activity_filter;
+		
 		//get the number of items to display
 		$dbprefix = elgg_get_config("dbprefix");
 		$offset = 0;
@@ -61,28 +37,20 @@
 		$sql .= " WHERE (entities1.container_guid in (" . implode(",", $group_guid) . ")";
 		$sql .= " OR {$dbprefix}river.object_guid IN (" . implode(",", $group_guid) . "))";
 		
-// 		if(!empty($activity_filter)){
-// 			$filter_wheres = array();
+		if(!empty($activity_filter) && is_string($activity_filter)){
+			list($type, $subtype) = explode(",", $activity_filter);
 			
-// 			foreach($activity_filter as $filter){
-// 				list($type, $subtype) = explode(",", $filter);
+			if(!empty($type)){
+				$filter_where = " ({$dbprefix}river.type = '" . sanitise_string($type) . "'";
 				
-// 				if(!empty($type)){
-// 					$filter_where = " ({$CONFIG->dbprefix}river.type = '" . sanitise_string($type) . "'";
-					
-// 					if(!empty($subtype)){
-// 						$filter_where .= " AND {$CONFIG->dbprefix}river.subtype = '" . sanitise_string($subtype) . "'";
-// 					}
-					
-// 					$filter_where .= ")";
-// 					$filter_wheres[] = $filter_where;
-// 				}
-// 			}
-			
-// 			if(!empty($filter_wheres)){
-// 				$sql .= " AND (" . implode(" OR ", $filter_wheres) . ")";
-// 			}
-// 		}
+				if(!empty($subtype)){
+					$filter_where .= " AND {$dbprefix}river.subtype = '" . sanitise_string($subtype) . "'";
+				}
+				
+				$filter_where .= ")";
+				$sql .= " AND " . $filter_where;
+			}
+		}
 		
 		$sql .= " AND " . get_access_sql_suffix("entities1");
 		$sql .= " ORDER BY {$dbprefix}river.posted DESC";
--- views/default/widgets/group_river_widget/edit.php
+++ views/default/widgets/group_river_widget/edit.php
@@ -13,47 +13,28 @@
 	
 	// filter activity
 	// get filter options
-// 	$filter_contents = array();
-// 	if (!empty($vars['config']->registered_entities)) {
-// 		foreach ($vars['config']->registered_entities as $type => $ar) {
-// 			if($type == "user"){
-// 				continue;
-// 			} else {
-// 				if (count($vars['config']->registered_entities[$type])) {
-// 					foreach ($vars['config']->registered_entities[$type] as $subtype) {
-// 						$keyname = 'item:' . $type . ':' . $subtype;
-// 						$filter_contents[$keyname] = "{$type},{$subtype}";
-// 					}
-// 				} else {
-// 					$keyname = 'item:' . $type;
-// 					$filter_contents[$keyname] = "{$type},";
-// 				}
-// 			}
-// 		}
-// 	}
+	$filter_contents = array("0" => elgg_echo("all"));
+	$registered_entities = elgg_get_config("registered_entities");
+	if (!empty($registered_entities)) {
+		foreach ($registered_entities as $type => $ar) {
+			if($type == "user"){
+				continue;
+			} else {
+				if (count($registered_entities[$type])) {
+					foreach ($registered_entities[$type] as $subtype) {
+						$keyname = 'item:' . $type . ':' . $subtype;
+						$filter_contents["{$type},{$subtype}"] = elgg_echo($keyname);
+					}
+				} else {
+					$keyname = 'item:' . $type;
+					$filter_contents["{$type},"] = elgg_echo($keyname);
+				}
+			}
+		}
+	}
 	
-// 	// build selector
-// 	$activity_filter = $widget->getMetadata("activity_filter");
-// 	if(empty($activity_filter)){
-// 		// fallback to old situation
-// 		$activity_filter = $widget->activity_filter;
-// 	}
+	$filter_selector = elgg_view("input/dropdown", array("name" => "params[activity_filter]", "value" => $widget->activity_filter, "options_values" => $filter_contents));
 	
-// 	if(!empty($activity_filter) && !is_array($activity_filter)){
-// 		$activity_filter = array($activity_filter);
-// 	} elseif(empty($activity_filter)){
-// 		$activity_filter = array();
-// 	}
-	
-// 	$filter_selector = elgg_view("input/hidden", array("internalname" => "params[activity_filter][]", "value" => "")); // needed to be able to store no selection
-// 	foreach($filter_contents as $label => $content){
-// 		if(in_array($content, $activity_filter)){
-// 			$filter_selector .= "<input type='checkbox' name='params[activity_filter][]' checked='checked' value='" . $content . "'>" . elgg_echo($label) . "<br />";
-// 		} else {
-// 			$filter_selector .= "<input type='checkbox' name='params[activity_filter][]' value='" . $content . "'>" . elgg_echo($label) . "<br />";
-// 		}
-// 	}
-
 	if($widget->context != "groups"){
 	    //the user of the widget
 		$owner = $widget->getOwnerEntity();
@@ -70,28 +51,14 @@
 		}
 		
 	    if($groups = elgg_get_entities_from_relationship($options)){
+	    	
 			// get groups
 	    	$group_options_values = array();
 			foreach($groups as $group){
-				$group_options_values[$group->getGUID()] = $group->name;
+				$group_options_values[$group->name] = $group->getGUID();
 			}
 			
 			natcasesort($group_options_values);
-
-			// get selected value(s)
-// 			if($group_guid = $widget->getMetadata("group_guid")){
-// 				if(!is_array($group_guid)){
-// 					$group_guid = array($group_guid);
-// 				}
-// 			} elseif($group_guid = $widget->group_guid) {
-// 				unset($widget->group_guid);
-// 				$group_guid = array($group_guid);
-// 	    	} else {
-// 				$group_guid = array();
-// 			}
-			
-// 			$group_guid = array_map("sanitise_int", $group_guid);
-			$group_guid = (int) $widget->group_guid;
 			
 			// make options
 			echo "<div>";
@@ -99,24 +66,14 @@
 			echo " " . elgg_view("input/dropdown", array("options" => range(5, 25, 5), "value" => $num_display, "name" => "params[num_display]"));
 			echo "</div>";
 			
-// 			echo "<div>";
-// 			echo elgg_echo("filter") . "<br />";
-// 			echo $filter_selector;
-// 			echo "</div>";
+			echo "<div>";
+			echo elgg_echo("filter") . "<br />";
+			echo $filter_selector;
+			echo "</div>";
 	
 			echo "<div>";
 			echo elgg_echo('widgets:group_river_widget:edit:group');
-// 			echo "<div>";
-// 			echo elgg_view("input/hidden", array("internalname" => "params[group_guid][]", "value" => ""));
-// 			foreach($group_options_values as $guid => $name){
-// 				$checked = "";
-// 				if(in_array($guid, $group_guid)){
-// 					$checked = "checked='checked'";
-// 				}
-// 				echo "<input type='checkbox' name='params[group_guid][]' value='" . $guid . "' " . $checked . " /> " . $name . "<br />";
-// 			}
-			echo "&nbsp;" . elgg_view("input/dropdown", array("name" => "params[group_guid]", "value" => $group_guid, "options_values" => $group_options_values));
-// 			echo "</div>";
+			echo elgg_view("input/checkboxes", array("name" => "params[group_guid]", "value" => $widget->group_guid, "options" => $group_options_values));
 			echo "</div>";
 		} else {
 			echo elgg_echo("widgets:group_river_widget:edit:no_groups");
@@ -124,11 +81,11 @@
 	} else {
 		echo "<div>";
 		echo elgg_echo('widgets:group_river_widget:edit:num_display'); 
-		echo " " . elgg_view("input/DROPdown", array("options" => range(5, 25, 5), "value" => $num_display, "name" => "params[num_display]"));
+		echo " " . elgg_view("input/dropdown", array("options" => range(5, 25, 5), "value" => $num_display, "name" => "params[num_display]"));
 		echo "</div>";
 		
-// 		echo "<div>";
-// 		echo elgg_echo("filter") . "<br />";
-// 		echo $filter_selector;
-// 		echo "</div>";
+		echo "<div>";
+		echo elgg_echo("filter") . "<br />";
+		echo $filter_selector;
+		echo "</div>";
 	}
