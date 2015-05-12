e299bc1cfc947f98e957f5a8f904d8f754134c22
Jerome Bakker <jeabakker@coldtrick.com>
2015-05-12 14:51:42 +0200

    group tool moved to checkboxes

--- views/default/group_tools/elements/group_tool.php
+++ views/default/group_tools/elements/group_tool.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Display one group tool option
- * 
+ *
  * @uses $vars['group_tool'] the group tool options
  * @uses $vars['value']      the current value of this option
  * @uses $vars['name']       (optional) the name of the input
@@ -13,30 +13,23 @@ $value = elgg_extract("value", $vars);
 
 $group_tool_toggle_name = elgg_extract("name", $vars, $group_tool->name . "_enable");
 
-$class = elgg_extract("class", $vars);
-if (!empty($class)) {
-	$class = "class='" . $class . "'";
-}
+$attr = array(
+	'class' => elgg_extract("class", $vars)
+);
 
-echo "<div " . $class . ">";
-
-echo elgg_view("input/radio", array(
-	"name" => $group_tool_toggle_name,
-	"value" => $value,
-	"options" => array(
-		elgg_echo("option:yes") => "yes",
-		elgg_echo("option:no") => "no",
-	),
-	"align" => "horizontal",
-	"class" => "float-alt"
+$content = elgg_view('input/checkbox', array(
+	'name' => $group_tool_toggle_name,
+	'value' => 'yes',
+	'default' => 'no',
+	'checked' => ($value === 'yes') ? true : false,
+	'label' => $group_tool->label,
+	'class' => 'mrs'
 ));
-
-echo "<label>" . $group_tool->label . "</label><br />";
 
 // optional description
 $lan_key = $group_tool->name . ":group_tool_option:description";
-if (elgg_echo($lan_key) != $lan_key) {
-	echo elgg_view("output/longtext", array("value" => elgg_echo($lan_key), "class" => "elgg-quiet mtn"));
+if (elgg_language_key_exists($lan_key)) {
+	$content .= elgg_view("output/longtext", array("value" => elgg_echo($lan_key), "class" => "elgg-quiet mtn"));
 }
 
-echo "</div>";
+echo elgg_format_element('div', $attr, $content);
