ColdTrick/group_tools

commit 3c9bb5d2e2f729ff4f99a8bccbbde4ea7d6b3c68

Fix: missing comment river items in group activity widget (#129)

views/default/widgets/group_river_widget/content.php
@@ -44,7 +44,9 @@ if ($limit < 1) {
 $sql = "SELECT {$dbprefix}river.*";
 $sql .= " FROM {$dbprefix}river";
 $sql .= " INNER JOIN {$dbprefix}entities AS entities1 ON {$dbprefix}river.object_guid = entities1.guid";
+$sql .= " LEFT JOIN {$dbprefix}entities AS entities2 ON {$dbprefix}river.target_guid = entities2.guid";
 $sql .= ' WHERE (entities1.container_guid in (' . implode(',', $group_guid) . ')';
+$sql .= " OR entities2.container_guid IN (" . implode(',', $group_guid) . ')';
 $sql .= " OR {$dbprefix}river.object_guid IN (" . implode(',', $group_guid) . '))';
 
 if (!empty($activity_filter) && is_string($activity_filter)) {

commit 9963802a5d6bc7d218630db3f490ab965a26ddf0

Fix regression introduced by commit 3c9bb5d

views/default/widgets/group_river_widget/content.php
@@ -41,46 +41,32 @@ if ($limit < 1) {
 	$limit = 10;
 }
 
-$sql = "SELECT {$dbprefix}river.*";
-$sql .= " FROM {$dbprefix}river";
-$sql .= " INNER JOIN {$dbprefix}entities AS entities1 ON {$dbprefix}river.object_guid = entities1.guid";
-$sql .= " LEFT JOIN {$dbprefix}entities AS entities2 ON {$dbprefix}river.target_guid = entities2.guid";
-$sql .= ' WHERE (entities1.container_guid in (' . implode(',', $group_guid) . ')';
-$sql .= " OR entities2.container_guid IN (" . implode(',', $group_guid) . ')';
-$sql .= " OR {$dbprefix}river.object_guid IN (" . implode(',', $group_guid) . '))';
+// set river options
+$options = [
+        'pagination' => false,
+	'limit' => $limit,
+	'offset' => $offset,
+        'distinct' => false,
+	'joins' => [
+		"JOIN {$dbprefix}entities e1 ON e1.guid = rv.object_guid",
+		"LEFT JOIN {$dbprefix}entities e2 ON e2.guid = rv.target_guid",
+	],
+	'wheres' => [
+		'(e1.container_guid IN (' . implode(',', $group_guid) . ')' . ' OR e1.guid IN (' . implode(',', $group_guid) . ')' . ' OR e2.container_guid IN (' . implode(',', $group_guid) . ')' . ')',
+	],
+	'no_results' => elgg_echo('widgets:group_river_widget:view:noactivity'),
+];
 
 if (!empty($activity_filter) && is_string($activity_filter)) {
 	list($type, $subtype) = explode(',', $activity_filter);
 	
 	if (!empty($type)) {
-		$filter_where = " ({$dbprefix}river.type = '" . sanitise_string($type) . "'";
+		$options['type'] = sanitise_string($type);
 		
 		if (!empty($subtype)) {
-			$filter_where .= " AND {$dbprefix}river.subtype = '" . sanitise_string($subtype) . "'";
-		}
-		
-		$filter_where .= ')';
-		$sql .= ' AND ' . $filter_where;
+			$options['subtype'] = sanitise_string($subtype);
+		}		
 	}
 }
 
-$sql .= ' AND ' . _elgg_get_access_where_sql(['table_alias' => 'entities1']);
-$sql .= " ORDER BY {$dbprefix}river.posted DESC";
-$sql .= " LIMIT {$offset},{$limit}";
-
-$items = get_data($sql, '_elgg_row_to_elgg_river_item');
-if (empty($items)) {
-	echo elgg_echo('widgets:group_river_widget:view:noactivity');
-	return;
-}
-
-$options = [
-	'pagination' => false,
-	'count' => count($items),
-	'items' => $items,
-	'list_class' => 'elgg-list-river elgg-river',
-	'limit' => $limit,
-	'offset' => $offset,
-];
-
-echo elgg_view('page/components/list', $options);
+echo elgg_list_river($options);