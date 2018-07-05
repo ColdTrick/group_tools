83169e28f5ea398b79f0e59fe6597dcbe9c9cc47
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-07-05 12:20:02 +0200

    chore: little bit of code cleanup

--- views/default/group_tools/forms/notifications.php
+++ views/default/group_tools/forms/notifications.php
@@ -3,24 +3,15 @@
  * Set notification settings of group members
  */
 
-$user = elgg_get_logged_in_user_entity();
-if (empty($user) || !$user->isAdmin()) {
-	// only site admins can do this
+if (!elgg_is_admin_logged_in()) {
 	return;
 }
 
 $group = elgg_extract('entity', $vars);
-if (!($group instanceof ElggGroup)) {
+if (!$group instanceof \ElggGroup) {
 	return;
 }
 
-// start building content
-$title = elgg_echo('group_tools:notifications:title');
+$content = elgg_view_form('group_tools/admin/notifications', [], $vars);
 
-// notification settings
-$content = elgg_view_form('group_tools/admin/notifications', [], [
-	'entity' => $group,
-]);
-
-// echo content
-echo elgg_view_module('info', $title, $content);
+echo elgg_view_module('info', elgg_echo('group_tools:notifications:title'), $content);
