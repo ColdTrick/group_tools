a893eeecaa0439f3c11f5c1d5fea1518bc45fb91
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-07-05 12:12:21 +0200

    chore: moved welcome message form to correct location

--- views/default/group_tools/forms/welcome_message.php
+++ views/default/group_tools/forms/welcome_message.php
@@ -9,32 +9,6 @@ if (!($group instanceof ElggGroup) || !$group->canEdit()) {
 	return;
 }
 
-$title = elgg_echo('group_tools:welcome_message:title');
+$form = elgg_view_form('group_tools/welcome_message', [], $vars);
 
-// build form
-$form_body = elgg_format_element('div', [], elgg_echo('group_tools:welcome_message:description'));
-$form_body .= elgg_view('input/longtext', [
-	'name' => 'welcome_message',
-	'value' => $group->getPrivateSetting('group_tools:welcome_message'),
-]);
-
-$form_body .= elgg_format_element('div', ['class' => 'elgg-subtext'], elgg_view('output/longtext', [
-	'value' => elgg_echo('group_tools:welcome_message:explain', [
-		elgg_get_logged_in_user_entity()->name,
-		$group->name,
-		$group->getURL(),
-	]),
-]));
-
-$form_body .= '<div class="elgg-foot">';
-$form_body .= elgg_view('input/hidden', ['name' => 'group_guid', 'value' => $group->getGUID()]);
-$form_body .= elgg_view('input/submit', ['value' => elgg_echo('save')]);
-
-// make form
-$content = elgg_view('input/form', [
-	'action' => 'action/group_tools/welcome_message',
-	'body' => $form_body,
-]);
-
-// draw content
-echo elgg_view_module('info', $title, $content);
+echo elgg_view_module('info', elgg_echo('group_tools:welcome_message:title'), $form);
