4d5d561b9baade6beec05008825cde70c10c55cb
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-03-27 13:56:16 +0100

    chore: updated Dutch translations

--- languages/nl.php
+++ languages/nl.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * This file was created by Translation Editor v7.0-beta
- * On 2018-12-14 16:01
+ * On 2019-03-27 13:54
  */
 
 return array (
+  'group_tools:settings:simple_tool_presets' => 'Vereenvoudig groep tools preset selectie',
+  'group_tools:settings:simple_tool_presets:help' => 'Vereenvoudigd de groep tool preset selectie. Enkel de groep tool preset titel en omschrijving worden getoond. De individuele tools worden niet getoond. Ook kan er in deze eenvoudige modus geen individuele tools worden geselecteerd.',
   'group_tools:profile:field:group_tools_preset' => 'Groep tool set',
   'group_tools:menu:title:add:preset' => 'Creëer een %s groep',
   'admin:groups:featured' => 'Uitgelicht',
