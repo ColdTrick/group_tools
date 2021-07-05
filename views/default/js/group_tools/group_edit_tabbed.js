2ae4027e3f4f03ee7749e836b3ff06770c8980e8
Jerome Bakker <jeabakker@coldtrick.com>
2021-07-05 11:31:39 +0200

    chore: removed debug code

--- views/default/js/group_tools/group_edit_tabbed.js
+++ views/default/js/group_tools/group_edit_tabbed.js
@@ -12,11 +12,11 @@ define(function (require) {
 
 		// hide everything
 		var $content = $('.elgg-layout-content');
-//		$content.find('> form, > div:not(.elgg-message)').hide();
+		$content.find('> form, > div:not(.elgg-message)').hide();
 		$('.group-tools-group-edit-section').hide();
 		
 		var link = $(this).children('a').attr('href');
-		console.log(link);
+		
 		switch (link) {
 			case '#group-tools-group-edit-profile':
 				$content.find('> form').show();
@@ -24,7 +24,6 @@ define(function (require) {
 
 				break;
 			case '#group-tools-group-edit-reason':
-				console.log(link);
 				$content.find('> form').show();
 				$('#group-tools-group-edit-reason').show();
 				
