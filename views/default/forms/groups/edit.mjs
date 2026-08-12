3ba9d030cc597b4f3746f02352a15b46e6fae19d
Jerôme Bakker <jeabakker@coldtrick.com>
2026-08-12 14:13:27 +0200

    added: validation of required fields on group edit before switching tab

--- views/default/forms/groups/edit.mjs
+++ views/default/forms/groups/edit.mjs
@@ -21,6 +21,47 @@ function add_group_suggestions() {
 	});
 }
 
+function check_required_fields(event) {
+	const $tab = $(this).parent();
+	if ($tab.hasClass('elgg-state-selected') || $tab.parent().find('li.elgg-state-selected').length === 0) {
+		// clicking on current selected tab
+		// or no tab selected yet
+		return;
+	}
+	
+	// validate form
+	const $form = $(this).closest('.elgg-form-groups-edit');
+	const $active_section = $form.find('.elgg-tabs-content > .elgg-state-active');
+	
+	const $inputs = $active_section.find('input, select, textarea');
+	let valid = true;
+	$inputs.each(function(index, elem) {
+		if (!elem.willValidate) {
+			// this element will not be validated (eg button)
+			return;
+		}
+		
+		if ($(elem).is(':hidden')) {
+			// input is not shown (eg conditional section)
+			return;
+		}
+		
+		if (!elem.reportValidity()) {
+			valid = false;
+			
+			return false;
+		}
+	});
+	
+	if (!valid) {
+		event.preventDefault();
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+		
+		return false;
+	}
+}
+
 var inputTimeout;
 $(document).on('input', 'form.elgg-form-groups-edit input[name="name"]', function(event) {
 	clearTimeout(inputTimeout);
@@ -39,3 +80,5 @@ $(document).on('change', '#groups-membership', function() {
 		$('#group-tools-join-motivation').hide();
 	}
 });
+
+$('body').on('click', '.elgg-form-groups-edit .elgg-components-tab a', check_required_fields); // register on body to be before tab switch in page/components/tabs.js
